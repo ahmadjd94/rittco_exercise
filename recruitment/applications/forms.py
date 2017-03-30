7c2e54b5679951da6f2085a0296504bef1147863
banana <ahmadjd94@bitbucket.org>
2017-03-30 20:32:53 +0200

    added the application form, needs validations

---
+++ recruitment/applications/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Application
+
+class ApplicationForm(forms.ModelForm):
+  """docstring for ClassName"""
+  class Meta:
+    model=Application
+    fields = ['applicant_name', 'applicant_gender',
+    "applicant_birthdate",
+    "applicant_nationality",
+    "applicant_residency",
+    "applicant_major",
+    "applicant_school",
+    "applicant_graduation_date",
+    "applicant_email",
+    "applicant_photo",
+    "applicant_about"]
--- recruitment/applications/views.py
+++ recruitment/applications/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-
+from .forms import ApplicationForm
+from .models import Application
 # Create your views here.
 
 from django.http import HttpResponse
 
 def index(request):
   return HttpResponse("<h1>WTF</h1>")
+def new (request):
+  if request.method =="POST":
+    form = ApplicationForm(request.POST)
+    application = form.save(commit=False)
+    application.save()
+  else:
+    form = ApplicationForm()
+    return render(request,'applications/application.html',{"form":form })
