3af9fa1524f17447b2b2b7d97ef7889812074344
banana <ahmadjd94@bitbucket.org>
2017-03-31 20:47:37 +0300

    major fixes

--- recruitment/applications/models.py
+++ recruitment/applications/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from django_countries.fields import CountryField
+
 # Create your models here.
 
 class Application(models.Model):
@@ -10,8 +12,8 @@ class Application(models.Model):
     applicant_name = models.TextField()
     applicant_gender = models.CharField(max_length = 6)
     applicant_birthdate = models.DateField(null = True)
-    applicant_nationality = models.TextField(null = True)
-    applicant_residency = models.TextField(null = True)
+    applicant_nationality = CountryField(null =True,blank_label= ("(SELECT NATIONALITY)"))
+    applicant_residency = CountryField(null =True,blank_label= ("(SELECT RESIDNECY)"))
     applicant_major = models.TextField(null = True)
     applicant_school = models.TextField(null = True)
     applicant_graduation_date = models.DateField(null = True)
--- recruitment/applications/views.py
+++ recruitment/applications/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from .forms import ApplicationForm
 from .models import Application
-
-
 from django.http import HttpResponse,HttpResponseRedirect
 
 def index(request):
   return HttpResponse("<h1>WTF</h1>")
+
 def success(request):
   return HttpResponse(render(request,"applications/success.html"))
+
 def new (request):
   if request.method =="POST":
     form = ApplicationForm(request.POST)
