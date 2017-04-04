b585285fb8b76408c60743b1ec1594e3bb7f1f9f
banana <ahmadjd94@bitbucket.org>
2017-04-04 19:14:18 +0300

    added a simple test

--- recruitment/applications/tests.py
+++ recruitment/applications/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from applications.models import Application
 
-# Create your tests here.
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        Application.objects.create(applicant_name="ahmad ", applicant_gender="male",applicant_nationality="JO",applicant_residency="JO",applicant_major="CS",applicant_school="PSUT",applicant_email="ahmadjd94@gmail.com",applicant_about="hello world")
+      
+
+    def test(self):
+        ahmad = Application.objects.get(applicant_name="ahmad")
+    #failing test 
