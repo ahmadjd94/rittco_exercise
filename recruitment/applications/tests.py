a5ccb109b04b664b69a169dbecc516ce5e14250d
banana <ahmadjd94@bitbucket.org>
2017-04-04 19:18:32 +0300

    cleanup

--- recruitment/applications/tests.py
+++ recruitment/applications/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from applications.models import Application
 
-class AnimalTestCase(TestCase):
+class ApplicationTestCase(TestCase):
     def setUp(self):
         Application.objects.create(applicant_name="ahmad ", applicant_gender="male",applicant_nationality="JO",applicant_residency="JO",applicant_major="CS",applicant_school="PSUT",applicant_email="ahmadjd94@gmail.com",applicant_about="hello world")
       
